8baf0482daca4bab08f27a4c951f0310d885f9df
Jakub Zalas <jakub@zalas.pl>
2021-11-11 13:58:33 +0000

    Clone via git@

--- pull.sh
+++ pull.sh
@@ -3,7 +3,7 @@ set -eou pipefail
 
 XOOM_PLATFORM_PATH=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)
 XOOM_HOME=$(cd -- "$XOOM_PLATFORM_PATH/.." >/dev/null 2>&1 ; pwd -P)
-XOOM_GITHUB_REPO="https://github.com/vlingo/{PROJECT}.git"
+XOOM_GITHUB_REPO="git@github.com:vlingo/{PROJECT}.git"
 XOOM_PROJECTS=($@)
 
 all_projects=(
